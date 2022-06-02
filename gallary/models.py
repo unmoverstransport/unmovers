da55bfc71dbb2f83ec89a838dafce19dbe0b2313
Eric Lekwane <unmoverstransport@gmail.com>
2022-06-02 11:47:41 +0200

    last try

--- gallary/models.py
+++ gallary/models.py
@@ -1,9 +1,7 @@
 #// imports 
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-import dropbox
 
-dxv = dropbox.Dropbox('sl.BIwoH7kttAAtVu0Qxbx8AUoImi34-duNu9ALsTKS_Y5_UIxISJ1hu5lCoiUQI7Jho-8d2RtXOBXLgfm4Xu5TIRQOpGjAcRrQD1UO67Dy3pmEYv3CMQ2SxwD0nRSh6pz_rd85Czh7Tec')
 
 
 #// upload function 
@@ -13,7 +11,7 @@ def upload_to_gallary(instance, filename):
 #// gallary model
 class ItemGallaryModel(models.Model):
     
-    image_gallary = models.ImageField(_("image_gallary"), upload_to=dxv.files_upload)
+    image_gallary = models.ImageField(upload_to=upload_to_gallary)
     # created at 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
